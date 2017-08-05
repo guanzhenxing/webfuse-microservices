3b8fbfa5db70c80d6278b12cb8baa69996000c2d
guanzhenxing <guan.zhenxing@foxmail.com>
2017-08-05 09:11:28 +0800

    添加ms-service-starter

---
+++ ms-service-starter/src/main/java/network/swan/ms/Application.java
@@ -0,0 +1,23 @@
+package network.swan.ms;
+/**
+ * Created by guanzhenxing on 2017/8/5.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+
+@SpringCloudApplication
+@EnableFeignClients
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.REGEX,
+                pattern = "network.swan.ms.config.ext.*"))
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ ms-service-starter/src/main/java/network/swan/ms/config/FeignLogConfiguration.java
@@ -0,0 +1,24 @@
+package network.swan.ms.config;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Feign的日志配置。<br/>
+ * 默认的Feign是不记录日志的，所以这边需要设置日志的级别<br/>
+ * <li>NONE：不记录日志</li>
+ * <li>BASIC：仅记录请求方法、URL、响应状态代码以及执行时间</li>
+ * <li>HEADERS：记录BASIC级别的基础上，记录请求和响应的header</li>
+ * <li>FULL：记录请求和响应的header，body和元数据</li>
+ * <p>
+ * Created by guanzhenxing on 2017/8/5.
+ */
+@Configuration
+public class FeignLogConfiguration {
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+}
