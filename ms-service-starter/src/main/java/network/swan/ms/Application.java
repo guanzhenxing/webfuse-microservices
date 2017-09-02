9a932d22572731546ec61fd484de3b874e62ab8d
zhenxing <guan.zhenxing@foxmail.com>
2017-09-02 08:19:41 +0800

    添加gitignore

--- ms-service-starter/src/main/java/network/swan/ms/Application.java
+++ ms-service-starter/src/main/java/network/swan/ms/Application.java
@@ -8,9 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringCloudApplication
 @EnableFeignClients
+@EnableAspectJAutoProxy(exposeProxy = true)
 @ComponentScan(excludeFilters = @ComponentScan.Filter(IgnoreDuringSpringScan.class))
 public class Application {
 
