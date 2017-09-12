7b315a82523ce4c8d734cef88ee6a135b51b059b
zhenxing <guan.zhenxing@foxmail.com>
2017-09-12 23:24:15 +0800

    disable page caching

--- ms-uaa-service/src/main/java/network/swan/uaa/configuration/security/WebSecurityConfig.java
+++ ms-uaa-service/src/main/java/network/swan/uaa/configuration/security/WebSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -64,14 +65,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http
                 .cors().and()
-                .csrf().disable()
-                .exceptionHandling()
-                .and()
+                .csrf().disable()   // we don't need CSRF because our token is invulnerable
+                .exceptionHandling() .and()
+                // don't create session
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
                 .antMatchers("/**").authenticated()
                 .and()
                 .httpBasic();
 
+        // disable page caching
+        http.headers().cacheControl();
 
     }
 
