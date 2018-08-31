c19399d402cb2fd42652a32cf21d6c63ac16ddb4
Jesen <guan.zhenxing@foxmail.com>
2018-08-31 21:24:16 +0800

    重新规划项目，重新划分类包

---
+++ ext-security/src/main/java/cn/webfuse/framework/ExtSecurityApplication.java
@@ -0,0 +1,12 @@
+package cn.webfuse.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtSecurityApplication.class, args);
+    }
+}
