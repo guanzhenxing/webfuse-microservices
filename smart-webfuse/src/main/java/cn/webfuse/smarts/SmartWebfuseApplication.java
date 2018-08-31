6fd5e3a37e9f7425da82a143c03889486b78b85f
Jesen <guan.zhenxing@foxmail.com>
2018-08-31 21:35:31 +0800

    添加一个示例项目

---
+++ smart-webfuse/src/main/java/cn/webfuse/smarts/SmartWebfuseApplication.java
@@ -0,0 +1,12 @@
+package cn.webfuse.smarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWebfuseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartWebfuseApplication.class, args);
+    }
+}
---
+++ smart-webfuse/src/test/java/cn/webfuse/smarts/SmartWebfuseApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.webfuse.smarts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartWebfuseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
