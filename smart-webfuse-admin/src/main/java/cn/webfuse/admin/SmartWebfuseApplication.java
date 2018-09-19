7f050620b4d6f6a3673e1b04d25677dc785b4dc2
Jesen <guan.zhenxing@foxmail.com>
2018-09-19 21:35:39 +0800

    一些引用的修改

--- smart-webfuse-admin/src/main/java/cn/webfuse/admin/SmartWebfuseApplication.java
+++ smart-webfuse-admin/src/main/java/cn/webfuse/admin/SmartWebfuseApplication.java
@@ -1,11 +1,13 @@
 package cn.webfuse.admin;
 
+import cn.webfuse.framework.logging.access.EnableAccessLogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(scanBasePackages = {"cn.webfuse"})
 @EnableAsync
+@EnableAccessLogger
 public class SmartWebfuseApplication {
 
     public static void main(String[] args) {
