7be11cf6714e53c13fbd29430315d857d4e706b0
Jesen <guan.zhenxing@foxmail.com>
2018-11-01 09:46:50 +0800

    系统启动

---
+++ smart-webfuse-demos/smart-webfuse-demos-mybatis-interceptor/src/main/java/cn/webfuse/App.java
@@ -0,0 +1,13 @@
+package cn.webfuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@MapperScan("cn.webfuse.**.mapper.**")
+@SpringBootApplication(scanBasePackages = {"cn.webfuse"})
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
