3cad3590e2e49c0efa6665569def8dc6975802a5
Jesen <guan.zhenxing@foxmail.com>
2018-11-21 16:51:48 +0800

    api版本管理

---
+++ webfuse-commons/webfuse-commons-version/src/main/java/cn/webfuse/framework/web/version/ApiVersion.java
@@ -0,0 +1,16 @@
+package cn.webfuse.framework.web.version;
+
+import org.springframework.web.bind.annotation.Mapping;
+
+import java.lang.annotation.*;
+
+/**
+ * 接口版本标识注解
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Mapping
+public @interface ApiVersion {
+    int value();
+}
