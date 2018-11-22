fc83d510882910b17a0667431e4894d099c63940
Jesen <guan.zhenxing@foxmail.com>
2018-11-22 17:46:16 +0800

    quartz-注解形式

--- smart-webfuse-demos/smart-webfuse-demos-quartz/src/main/java/cn/webfuse/demos/quartz/Application.java
+++ smart-webfuse-demos/smart-webfuse-demos-quartz/src/main/java/cn/webfuse/demos/quartz/Application.java
@@ -1,15 +1,11 @@
 package cn.webfuse.demos.quartz;
 
-import org.quartz.DisallowConcurrentExecution;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@DisallowConcurrentExecution
-@ImportResource("applicationContext.xml")
 public class Application {
 
     public static void main(String[] args) {
---
+++ smart-webfuse-demos/smart-webfuse-demos-quartz/src/main/java/cn/webfuse/demos/quartz/config/QuartzSchedulerConfig.java
@@ -0,0 +1,130 @@
+package cn.webfuse.demos.quartz.config;
+
+import cn.webfuse.demos.quartz.job.Job1;
+import cn.webfuse.demos.quartz.job.Job2;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.CronTrigger;
+import org.quartz.JobDetail;
+import org.quartz.spi.JobFactory;
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.Properties;
+
+@Configuration
+@ConditionalOnProperty(name = "quartz.enabled")
+@Slf4j
+public class QuartzSchedulerConfig {
+
+    private static final String QUARTZ_PROPERTIES_NAME = "/quartz.properties";
+
+    @Autowired
+    private DataSource dataSource;
+
+    /**
+     * 获得配置文件
+     */
+    @Bean
+    public Properties quartzProperties() throws IOException {
+        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+        propertiesFactoryBean.setLocation(new ClassPathResource(QUARTZ_PROPERTIES_NAME));
+        propertiesFactoryBean.afterPropertiesSet();
+        return propertiesFactoryBean.getObject();
+    }
+
+    /**
+     * 配置JobFactory
+     */
+    @Bean
+    public JobFactory jobFactory(ApplicationContext applicationContext) {
+        AutowiringSpringBeanJobFactory jobFactory = new AutowiringSpringBeanJobFactory();
+        jobFactory.setApplicationContext(applicationContext);
+        return jobFactory;
+    }
+
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean(JobFactory jobFactory, CronTrigger[] cronTrigger, JobDetail[] jobDetails) {
+        SchedulerFactoryBean factoryBean = new SchedulerFactoryBean();
+        try {
+            factoryBean.setQuartzProperties(quartzProperties());
+            factoryBean.setDataSource(dataSource);
+            factoryBean.setJobFactory(jobFactory);
+            factoryBean.setTriggers(cronTrigger);
+            factoryBean.setJobDetails(jobDetails);
+            factoryBean.setOverwriteExistingJobs(true);
+        } catch (Exception e) {
+            log.error("加载 {} 配置文件失败.", QUARTZ_PROPERTIES_NAME, e);
+            throw new RuntimeException("加载配置文件失败", e);
+        }
+
+        return factoryBean;
+    }
+
+    @Bean(name = "job1Trigger")
+    public CronTriggerFactoryBean job1Trigger(@Qualifier("job1Detail") JobDetail jobDetail) {
+        CronTriggerFactoryBean cronTriggerFactoryBean = new CronTriggerFactoryBean();
+        cronTriggerFactoryBean.setJobDetail(jobDetail);
+        cronTriggerFactoryBean.setCronExpression("0/15 * * * * ?");
+        return cronTriggerFactoryBean;
+    }
+
+    @Bean(name = "job1Detail")
+    public JobDetailFactoryBean job1Detail() {
+        JobDetailFactoryBean jobDetailFactoryBean = new JobDetailFactoryBean();
+        jobDetailFactoryBean.setJobClass(Job1.class);
+        jobDetailFactoryBean.setDurability(true);
+        return jobDetailFactoryBean;
+    }
+
+    @Bean(name = "job2Trigger")
+    public CronTriggerFactoryBean job2Trigger(@Qualifier("job2Detail") JobDetail jobDetail) {
+        CronTriggerFactoryBean cronTriggerFactoryBean = new CronTriggerFactoryBean();
+        cronTriggerFactoryBean.setJobDetail(jobDetail);
+        cronTriggerFactoryBean.setCronExpression("0/5 * * * * ?");
+        return cronTriggerFactoryBean;
+    }
+
+    @Bean(name = "job2Detail")
+    public JobDetailFactoryBean job2Detail() {
+        JobDetailFactoryBean jobDetailFactoryBean = new JobDetailFactoryBean();
+        jobDetailFactoryBean.setJobClass(Job2.class);
+        jobDetailFactoryBean.setDurability(true);
+        return jobDetailFactoryBean;
+    }
+
+
+    class AutowiringSpringBeanJobFactory extends SpringBeanJobFactory implements ApplicationContextAware {
+
+        private transient AutowireCapableBeanFactory beanFactory;
+
+        @Override
+        public void setApplicationContext(final ApplicationContext context) {
+            beanFactory = context.getAutowireCapableBeanFactory();
+        }
+
+        @Override
+        protected Object createJobInstance(final TriggerFiredBundle bundle)
+                throws Exception {
+            final Object job = super.createJobInstance(bundle);
+            beanFactory.autowireBean(job);
+            return job;
+        }
+    }
+
+
+}
