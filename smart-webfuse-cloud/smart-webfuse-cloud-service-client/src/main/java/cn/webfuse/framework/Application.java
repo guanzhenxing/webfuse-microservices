79a6d0c131f1a62279b5383e079a74325ec7eb07
Jesen <guan.zhenxing@foxmail.com>
2018-11-23 11:40:16 +0800

    熔断器

--- smart-webfuse-cloud/smart-webfuse-cloud-service-client/src/main/java/cn/webfuse/framework/Application.java
+++ smart-webfuse-cloud/smart-webfuse-cloud-service-client/src/main/java/cn/webfuse/framework/Application.java
@@ -1,12 +1,10 @@
 package cn.webfuse.framework;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
-@EnableDiscoveryClient
+@SpringCloudApplication
 @EnableFeignClients
 public class Application {
 
