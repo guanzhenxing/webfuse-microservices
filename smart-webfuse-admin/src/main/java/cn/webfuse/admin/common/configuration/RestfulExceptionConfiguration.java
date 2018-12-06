1ab9ce1b8fc1f305fa8d0144e3f223d75d83a7a9
Jesen <guan.zhenxing@foxmail.com>
2018-12-06 16:12:50 +0800

    服务跟踪zipkin

--- smart-webfuse-admin/src/main/java/cn/webfuse/admin/common/configuration/RestfulExceptionConfiguration.java
+++ smart-webfuse-admin/src/main/java/cn/webfuse/admin/common/configuration/RestfulExceptionConfiguration.java
@@ -1,12 +1,12 @@
 package cn.webfuse.admin.common.configuration;
 
-import cn.webfuse.framework.exception.configuration.AbstractBaseRestfulExceptionConfig;
+import cn.webfuse.framework.config.AbstractBaseRestfulExceptionConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Map;
 
 @Configuration
-public class RestfulExceptionConfig extends AbstractBaseRestfulExceptionConfig {
+public class RestfulExceptionConfiguration extends AbstractBaseRestfulExceptionConfiguration {
     @Override
     public Map<String, String> getCustomExceptionMappingDefinitions() {
         return null;
---
+++ smart-webfuse-cloud/smart-webfuse-cloud-service-zipkin-server/src/main/java/cn/webfuse/cloud/zipkin/Application.java
@@ -0,0 +1,14 @@
+package cn.webfuse.cloud.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin2.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
