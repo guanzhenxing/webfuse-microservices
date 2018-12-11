75fe3da12bc3a4c4acd0b6a3b09b938bc70fbe7a
Jesen <guan.zhenxing@foxmail.com>
2018-12-11 11:13:01 +0800

    docker试验

--- smart-webfuse-demos/smart-webfuse-demos-docker/src/main/java/cn/webfuse/Application.java
+++ smart-webfuse-demos/smart-webfuse-demos-docker/src/main/java/cn/webfuse/Application.java
@@ -1,12 +1,30 @@
 package cn.webfuse;
 
+import org.apache.commons.collections.map.SingletonMap;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @SpringBootApplication(scanBasePackages = {"cn.webfuse"})
-public class Application
-{
+@RestController
+@RequestMapping("/test")
+public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Value("${demos.value1}")
+    private String value1;
+
+    @GetMapping("/")
+    public Map<String, String> test() {
+        return new SingletonMap("v1", value1);
+    }
+
 }
