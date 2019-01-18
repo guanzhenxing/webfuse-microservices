38c69f1f7fedeb0db578a9a34e970289367f4eed
Jesen <guan.zhenxing@foxmail.com>
2019-01-18 18:12:25 +0800

    日志功能测试

--- smart-webfuse-admin/src/main/java/cn/webfuse/admin/upms/TestController.java
+++ smart-webfuse-admin/src/main/java/cn/webfuse/admin/upms/TestController.java
@@ -1,6 +1,7 @@
 package cn.webfuse.admin.upms;
 
 import cn.webfuse.admin.upms.model.GroupRole;
+import cn.webfuse.framework.logging.access.AccessLogger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.Date;
 public class TestController {
 
     @GetMapping("/one")
+    @AccessLogger("测试")
     public GroupRole getOne() {
 
 
