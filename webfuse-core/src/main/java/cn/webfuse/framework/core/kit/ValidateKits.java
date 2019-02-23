3a355495a8953dfd7f4fa2b086883f6edb84d515
Jesen <guan.zhenxing@foxmail.com>
2019-02-23 21:17:05 +0800

    一些工具类

--- webfuse-core/src/main/java/cn/webfuse/framework/core/kit/ValidateKits.java
+++ webfuse-core/src/main/java/cn/webfuse/framework/core/kit/ValidateKits.java
@@ -3,7 +3,7 @@ package cn.webfuse.framework.core.kit;
 /**
  * 参数校验工具类
  * <p>
- * 参数校验统一使用Apache Common Lange Validate, 此处为补充一些缺少的
+ * 参数校验统一使用Apache Common Lang3 Validate, 此处为补充一些缺少的
  */
 public class ValidateKits {
 
